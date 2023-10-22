051252e18cab120df5d71df73ae303b81a4ebdff
NARajab <11211067@student.itk.ac.id>
2023-10-22 16:21:09 +0800

    modify index.js

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ const ApiError = require("./utils/apiError")
 const errorHandler = require("./controller/errorController")
 const router = require("./routes")
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(`${__dirname}/public`))
-
-app.set("views", __dirname + "/views")
-app.set("view engine", "ejs")
 
 app.use(morgan("dev"))
 app.use(router)
